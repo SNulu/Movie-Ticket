12a0d8d92a0d61f00cf494287d8b0812195cfe91
Snulu <Nnabwami@gmail.com>
2019-02-03 20:19:45 -0800

    create ticket objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,23 @@
 // Business Logic for AddressBook ---------
-function MovieTicket(){
-  this.name = "",
-  this.time = "",
-  this.age= 0
+function MovieTicket(name, time, age){
+  this.name = name,
+  this.time = time,
+  this.age= age
 }
 
-MovieTicket.prototype.addName = function (name) {
-  //contact.id = this.assignId();
-  this.name = name;
-}
 
-MovieTicket.prototype.addTime = function (time) {
-  //contactAddress.id = this.assignId();
-  this.time = time;
-}
+
 
 $(document).ready(function () {
 
   $("form#movie-Tickets").submit(function (event) {
     event.preventDefault();
     var movieName = $("#name").val();
-    console.log(movieName);
     var movieTime = $("#appt-time").val();
-    console.log(movieTime);
     var yourAge = $("#age").val();
-    console.log(yourAge);
+
+    var ticket = new MovieTicket(movieName, movieTime, yourAge);
+    console.log(ticket);
     console.log("Add")
   });
 });
