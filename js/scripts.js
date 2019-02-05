41df929c396e34ea8bcd4f3f9a1d82bcd17218eb
Snulu <Nnabwami@gmail.com>
2019-02-04 22:18:29 -0800

    create span tags in index.html, javascript return infor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
 // Business Logic for AddressBook ---------
-function MovieTicket(name, time, age){
+function MovieTicket(name, time, age) {
   this.name = name,
   this.time = time,
-  this.age= age
-}
+  this.age = age
+};
 
+MovieTicket.prototype.cost = function () {
+  var baseCost = 10;
+  if (this.age < 60) {
+    baseCost += 2.5;
+  }
+  if (parseInt(this.time.slice(0, 2)) >= 16) {
+    baseCost += 2.5;
+  }
+  return baseCost
+};
 
 
 
@@ -18,6 +28,10 @@ $(document).ready(function () {
 
     var ticket = new MovieTicket(movieName, movieTime, yourAge);
     console.log(ticket);
+    console.log(ticket.cost());
     console.log("Add")
+    $('.movie-name').text(ticket.name);
+    $('.movie-time').text(ticket.time);
+    $('.movie-price').text(ticket.cost());
   });
 });
